837346c11976b17114577beb12210f6e694fee06
Pedro Leal <pedroleal360@gmail.com>
2024-03-18 20:59:38 -0300

    Create Aula1.go

---
+++ FullCycle-Bootcamp/Aula1.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Person struct {
+	name string
+	age  int
+}
+
+func (p Person) Walk() {
+	fmt.Println(p.name + "andando")
+}
+
+func main() {
+	//fmt.Println("Hello, world!")
+	var pessoa Person
+	pessoa.name = "lucas"
+	pessoa.age = 25
+	//pessoa.Walk()
+	go cont(6)
+	go cont(6)
+	cont(6)
+}
+
+func cont(count int) {
+	for i := 0; i < count; i++ {
+		time.Sleep(1 * time.Second)
+		fmt.Println(i)
+	}
+
+}
